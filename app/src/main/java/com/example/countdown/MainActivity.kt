9d80d0ec9d0f0121835df567b053bc195b9268ea
Mohammad rasool <muhammedjaf33@gmail.com>
2024-06-05 10:38:46 +0300

    second commit add hours

--- app/src/main/java/com/example/countdown/MainActivity.kt
+++ app/src/main/java/com/example/countdown/MainActivity.kt
@@ -20,12 +20,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-
-
         binding.button.setOnClickListener {
             val minutes=if(binding.editText.text.toString().isEmpty()) 0 else binding.editText.text.toString().toInt()
             val seconds=if(binding.editText2.text.toString().isEmpty()) 0 else binding.editText2.text.toString().toInt()
-            val milliSecond=((minutes*60)+seconds)*1000L
+            val hours=if(binding.editText3.text.toString().isEmpty()) 0 else binding.editText3.text.toString().toInt()
+            val milliSecond=((hours*3600)+(minutes*60)+seconds)*1000L
             if(milliSecond>0){
                 startCountDown(milliSecond)
             }
@@ -33,20 +32,18 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this,"Please enter a valid time",Toast.LENGTH_SHORT).show()
             }
         }
-
     }
     private fun startCountDown(milliSecond:Long){
         countDownTimer?.cancel()
         countDownTimer=object :CountDownTimer(milliSecond,1000){
             override fun onTick(millisUntilFinished: Long) {
-                val minutes=(millisUntilFinished/1000)/60
-                val seconds=(millisUntilFinished/1000)%60
-                binding.textView.text="${(if (minutes>9) minutes  else "0"+minutes)}:${(if (seconds>9) seconds else "0"+seconds)}"
-
+                val hours = (millisUntilFinished / 1000) / 3600
+                val minutes = ((millisUntilFinished / 1000) / 60) % 60
+                val seconds = (millisUntilFinished / 1000) % 60
+                binding.textView.text="${(if(hours>9) hours else "0"+hours)}:${(if (minutes>9) minutes  else "0"+minutes)}:${(if (seconds>9) seconds else "0"+seconds)}"
             }
-
             override fun onFinish() {
-                binding.textView.text="00:00"
+                binding.textView.text="00:00:00"
                 val alarm:MediaPlayer=MediaPlayer.create(applicationContext,R.raw.alarm)
                 alarm.start()
 
